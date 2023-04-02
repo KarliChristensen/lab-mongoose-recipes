a384482ff28a282a97cf299d80d07a46b81cbce9
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-04-02 22:40:34 +0200

    Updated, issue with MongoDB connection

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: `Rum Punch`,
+      level: `Easy Peasy`,
+      ingredients: [
+        `Orange juice`,
+        `Lime juice`,
+        `Golden rum`,
+        `Sugar syrup`,
+        `Grenadine syrup`,
+        `Angostura bitters`,
+        `Ice cubes`,
+        `Grated nutmeg`,
+        `Orange slices`,
+        `Maraschino cherries`,
+      ],
+      cuisine: `Carribian`,
+      dishType: `Drink`,
+      image: `https://images.immediate.co.uk/production/volatile/sites/30/2020/08/rum-punch-5c8fd79.jpg?quality=90&webp=true&resize=300,272`,
+      duration: 5,
+      creator: `John Smith`,
+      created: Date.now(),
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((recipe) => {
+    console.log(`Recipe done: ${recipe.title}`);
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      console.log("-", recipe.title);
+    });
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log(`Recipe removed`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+  .finally(() => {
+    console.log(`Disconnected`);
+    mongoose.disconnect();
   });
